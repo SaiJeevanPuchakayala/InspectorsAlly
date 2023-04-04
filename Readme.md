44c1c2dbe91fe15853e8b478ccb3e953c168fe4b
saijeevanpuchakayala <saijeevan2002@gmail.com>
2023-04-04 11:43:10 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,54 @@
+
+# InspectorsAlly
+
+## Boost Your Quality Control with InspectorsAlly - The Ultimate AI-Powered Inspection App
+
+InspectorsAlly is a powerful AI-powered application designed to help businesses streamline their quality control inspections. With InspectorsAlly, companies can ensure that their products meet the highest standards of quality, while reducing inspection time and increasing efficiency.
+
+This advanced inspection app uses state-of-the-art computer vision algorithms and machine learning models to perform visual quality control inspections with unparalleled accuracy and speed. InspectorsAlly is capable of identifying even the slightest defects, such as scratches, dents, discolorations, and more.
+
+InspectorsAlly is easy to use, with a simple and intuitive interface that allows users to set up their inspection criteria quickly and easily. The application can be customized to fit the specific needs of any industry, with the ability to set inspection thresholds and adjust parameters as needed.
+
+With InspectorsAlly, businesses can optimize their quality control process and ensure that their products meet the highest standards of quality. Say goodbye to time-consuming and error-prone manual inspections and hello to faster, more accurate, and more efficient quality control inspections with InspectorsAlly.
+
+<br>
+<hr>
+<br>
+
+## ⭐ Anomaly Detection Dataset 
+
+> ### ⭐ Dataset Download Link: https://www.mvtec.com/company/research/datasets/mvtec-ad
+This is a 400-image dataset, that contains images of good items (labeled as class ‘Good’) and items with a defect (labeled as class ‘Anomaly’). Dataset is imbalanced — with more samples of good images than defective ones. Item in the image may be literally of any type and complexity — bottle, cable, pill, tile, leather, zipper, etc. Below is an example of how the dataset may look like.
+
+![sample dataset images](docs/overview_dataset.jpg)
+
+<br>
+<hr>
+<br>
+
+## ⭐ Development References:
+1. https://www.analyticsvidhya.com/blog/2021/06/transfer-learning-using-vgg16-in-pytorch/
+2. https://blog.paperspace.com/vgg-from-scratch-pytorch/
+
+<br>
+<hr>
+<br>
+
+## ⭐ Streamlit Deployment Configurations:
+```
+[theme]
+base="dark"
+
+[browser]
+gatherUsageStats = false
+```
+<br>
+<hr>
+<br>
+
+## ⭐ Deployment References:
+1. https://30days.streamlit.app/
+2. https://docs.streamlit.io/streamlit-community-cloud/get-started/deploy-an-app
+3. https://streamlit-cloud-example-apps-streamlit-app-sw3u0r.streamlit.app/?hsCtaTracking=28f10086-a3a5-4ea8-9403-f3d52bf26184|22470002-acb1-4d93-8286-00ee4f8a46fb
+4. https://docs.streamlit.io/library/advanced-features/configuration
+
---
+++ streamlitApp.py
@@ -0,0 +1,136 @@
+import streamlit as st
+import io
+import cv2
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from utils.dataloader import get_train_test_loaders
+from utils.model import CustomVGG
+
+
+from PIL import Image
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Set up the page layout
+st.set_page_config(page_title="InspectorsAlly", page_icon=":camera:")
+
+
+st.title("InspectorsAlly")
+
+st.caption(
+    "Boost Your Quality Control with InspectorsAlly - The Ultimate AI-Powered Inspection App"
+)
+
+st.write(
+    "Try clicking a product image and watch how an AI Model will detect the anomaly."
+)
+
+with st.sidebar:
+    img = Image.open("./docs/overview_dataset.jpg")
+    st.image(img)
+    st.subheader("About InspectorsAlly")
+    st.write(
+        "InspectorsAlly is a powerful AI-powered application designed to help businesses streamline their quality control inspections. With InspectorsAlly, companies can ensure that their products meet the highest standards of quality, while reducing inspection time and increasing efficiency."
+    )
+
+    st.write(
+        "This advanced inspection app uses state-of-the-art computer vision algorithms and machine learning models to perform visual quality control inspections with unparalleled accuracy and speed. InspectorsAlly is capable of identifying even the slightest defects, such as scratches, dents, discolorations, and more."
+    )
+
+
+# Define the functions to load images
+def load_uploaded_image(file):
+    img = Image.open(file)
+    return img
+
+
+# Set up the sidebar
+st.subheader("Select Image Input Method")
+input_method = st.radio(
+    "options", ["File Uploader", "Camera Input"], label_visibility="collapsed"
+)
+
+# Check which input method was selected
+if input_method == "File Uploader":
+    uploaded_file = st.file_uploader(
+        "Choose an image file", type=["jpg", "jpeg", "png"]
+    )
+    if uploaded_file is not None:
+        uploaded_file_img = load_uploaded_image(uploaded_file)
+        st.image(uploaded_file_img, caption="Uploaded Image", width=300)
+        st.success("Image uploaded successfully!")
+    else:
+        st.warning("Please upload an image file.")
+
+elif input_method == "Camera Input":
+    st.warning("Please allow access to your camera.")
+    camera_image_file = st.camera_input("Click an Image")
+    if camera_image_file is not None:
+        camera_file_img = load_uploaded_image(camera_image_file)
+        st.image(camera_file_img, caption="Camera Input Image", width=300)
+        st.success("Image clicked successfully!")
+    else:
+        st.warning("Please click an image.")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+data_folder = "data/"
+subset_name = "leather"
+data_folder = os.path.join(data_folder, subset_name)
+
+
+def Anomaly_Detection(image_path, root):
+    """
+    Given an image path and a trained PyTorch model, returns the predicted class and bounding boxes for any defects detected in the image.
+    """
+
+    batch_size = 1
+    threshold = 0.5
+
+    subset_name = "leather"
+    model_path = f"weights/{subset_name}_model.h5"
+    model = torch.load(model_path, map_location=device)
+
+    # Get the list of class names from the test loader
+    _, test_loader = get_train_test_loaders(root, batch_size=batch_size)
+    class_names = test_loader.dataset.classes
+
+    # Load the image and preprocess it
+    transform = transforms.Compose(
+        [transforms.Resize((224, 224)), transforms.ToTensor()]
+    )
+    image = transform(image_path).unsqueeze(0)
+
+    # Get the model's predictions for the image
+    with torch.no_grad():
+        output = model(image)
+    predicted_probabilities = torch.sigmoid(output).squeeze().cpu().numpy()
+
+    # Get the predicted class label and probability
+    predicted_class_index = np.argmax(predicted_probabilities)
+    predicted_class = class_names[predicted_class_index]
+
+    return predicted_class
+
+
+submit = st.button(label="Submit Product Image")
+if submit:
+    st.subheader("Output")
+    if input_method == "File Uploader":
+        img_file_path = uploaded_file_img
+    elif input_method == "Camera Input":
+        img_file_path = camera_file_img
+    prediction = Anomaly_Detection(img_file_path, data_folder)
+    with st.spinner(text="This may take a moment..."):
+        st.write(prediction)
